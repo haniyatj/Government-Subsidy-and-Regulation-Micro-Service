00fe0cf5f42e24b4b636d6307f0c181ba883f8c2
anumsajid13 <anumsajid13@gmail.com>
2024-12-05 20:20:00 +0500

    added env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 
 const mongoose = require('mongoose');
+require('dotenv').config(); 
+
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://i211209:Xg5lmHfaQt0GN7tW@agrilink.8s85i.mongodb.net/Agrilink", {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
